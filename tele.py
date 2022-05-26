692006e66d369b4856da2836fc9a074dfd6d2c5c
sherylau <99946084+sherylau@users.noreply.github.com>
2022-05-26 21:55:21 +0800

    Update tele.py

--- tele.py
+++ tele.py
@@ -5,7 +5,7 @@ from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
 
-updater = Updater("5340307669:AAHcHNj6Ir58_AfUdJVDRV-aJdPk0noOL04", use_context=True)
+updater = Updater("TOKEN", use_context=True)
 
 #message to be sent to user whenever they press start in the beginning
 def start(update: Update, context: CallbackContext):
