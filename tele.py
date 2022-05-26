e92e731ed82955ae1f20bf07a4f15a1e59c2e343
sherylau <99946084+sherylau@users.noreply.github.com>
2022-05-26 14:55:47 +0800

    Update tele.py

--- tele.py
+++ tele.py
@@ -5,17 +5,26 @@ from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
 
-updater = Updater("5340307669:AAHcHNj6Ir58_AfUdJVDRV-aJdPk0noOL04
-", use_context=True)
+updater = Updater("5340307669:AAHcHNj6Ir58_AfUdJVDRV-aJdPk0noOL04", use_context=True)
 
 #message to be sent to user whenever they press start in the beginning
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text(
-            "Hello! I’m FoodSaver, I’m here to reduce food wastage. Here are some commands to use the features I provide.
-/add: Everytime you purchase food, simply log in the food, the amount (in servings) and its expiry date. I’ll help to keep track of it and remind you to consume it before it expires!
-/consume: Everytime you consume a certain type of food, tell me exactly what you consumed, the amount and I’ll remove it from the list.
-/list: This allows you to view a list of all the available food you have at any point in time to prevent buying duplicates.
-")
+    update.message.reply_text("welcome")
 
-        
+def unknown(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "Sorry '%s' is not a valid command" % update.message.text)
+    
+def unknown_text(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "Sorry I can't recognize you , you said '%s'" % update.message.text)
+  
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
+updater.dispatcher.add_handler(MessageHandler(
+    Filters.command, unknown))  
+
+updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
+  
+updater.start_polling()                
         #new comment
