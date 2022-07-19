78dd1072ab437d928dea34394b5fa53c57e78e0c
sherylau <99946084+sherylau@users.noreply.github.com>
2022-07-19 21:02:47 +0800

    Create points_command.py

---
+++ points_command.py
@@ -0,0 +1,6 @@
+@bot.message_handler(commands=['points'])
+def points(message):
+    cur = conn.cursor()
+    cur.execute(f"SELECT points FROM users WHERE userID = {message.from_user.id}")
+    result = cur.fetchall()               
+    bot.send_message(message.from_user.id, f"You have accumulated {result[0][0]} points.")    
