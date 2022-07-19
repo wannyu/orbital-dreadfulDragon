710bfc5d47e1ed3fbbf2dfa041af70bb45e604c3
sherylau <99946084+sherylau@users.noreply.github.com>
2022-07-19 15:38:55 +0800

    Update packages.py

--- packages.py
+++ packages.py
@@ -1,1 +1,20 @@
+#for database connection
+import psycopg2
+from urllib.parse import urlparse
 
+#connect to our bot
+import telebot
+
+#packages needed
+from telebot import types
+
+import random
+import threading
+
+import datetime as dt
+from datetime import date
+from datetime import datetime
+from time import sleep
+import pytz
+
+import math
