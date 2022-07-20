48a749bf6cee57ac2ad0f134c0d9c5021477ab1d
wannyu <99942808+wannyu@users.noreply.github.com>
2022-07-20 23:03:33 +0800

    Update and rename create_table.sql to users_table.sql

---
+++ users_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    userID BIGINT PRIMARY KEY,
+    household INT NOT NULL,
+    points BIGINT NOT NULL,
+    startDate DATE NOT NULL,
+    weeklyServings INT NOT NULL,
+    servingLimit INT NOT NULL,
+    username VARCHAR(1000) NOT NULL, 
+    reminderFreq INT NOT NULL
+);
