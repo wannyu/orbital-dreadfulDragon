0ff4312ad8cce77cb1c4d97715fff20b736f8c2e
wannyu <99942808+wannyu@users.noreply.github.com>
2022-07-21 15:17:15 +0800

    threading helper functions

--- threading_functions.py
+++ threading_functions.py
@@ -1,16 +1,29 @@
+"""
+send message to all existing users, except those who stop bot
+"""
 def cus_send_message(id, text):
      try:
         bot.send_message(id, text)
      except Exception as e:
         print(e)
 
+"""
+helper function
+takes in the query result from reminders table and the current hour in 24h clock
+checks if the same message have been sent in the same hour
+return a boolean result to indicate if the message can be sent (if it hasnt sent in the same hour)
+"""
 def cansend(db_reminders_result, hour):
-    # determine if message should be sent, returns boolean
     index = (hour // 3) + 1
     return db_reminders_result[0][index] == 0
 
+"""
+helper function
+takes in the query result from reminders table, the current hour in 24h clock and user's reminder frequency (int)
+checks if the same message have been sent in the same hour and should be sent based on the reminder frequency
+return a boolean result to indicate if the message can and should be sent (if it hasnt sent in the same hour and user should recieve it in this hour)
+"""
 def canandshouldsend(db_reminders_result, hour, rem_freq):
-    # determine if message should be sent based on user's personal reminder freq, returns boolean
     timings_list = []
     if rem_freq == 1:
         timings_list = [0]
