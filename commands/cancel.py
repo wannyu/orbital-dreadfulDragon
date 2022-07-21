2798c28f866c7f47518e78d3c357505bd5e50b76
wannyu <99942808+wannyu@users.noreply.github.com>
2022-07-21 14:09:51 +0800

    /cancel

--- commands/cancel.py
+++ commands/cancel.py
@@ -1,3 +1,4 @@
 @bot.message_handler(commands=['cancel'])
 def cancel(message):
-    bot.send_message(message.from_user.id,"You are currently in the main menu and have no operation to cancel. \nPlease only use /cancel when you want to exit the /add, /addmany, /consume, /consumemany or /edit operation.")
+    bot.send_message(message.from_user.id,"""You are currently in the main menu and have no operation to cancel.
+    Please only use /cancel when you want to exit the /add, /addmany, /consume, /consumemany or /edit operation.""")
